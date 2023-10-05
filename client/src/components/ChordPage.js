2c9beafa044a14fb43ab4c292c57f6e7eb508e60
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-10-05 08:34:54 -0700

    edit chord form conditionally renders in ChordPage comp needs submit functionality and formatting

--- client/src/components/ChordPage.js
+++ client/src/components/ChordPage.js
@@ -1,11 +1,34 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { UserContext } from "./UserContext"
 
 export default function ChordPage({ chord }) {
   const {user, setUser} = useContext(UserContext)
-  // console.log(user)
+  const [editChord, setEditChord] = useState(false)
+  const [chordFormData, setChordFormData] = useState({
+    name: chord.name,
+    notes: chord.notes,
+    inversion: chord.inversion,
+    comments: chord.comments,
+    image_url: chord.image_url,
+    user_id: chord.user_id,
+    song_id: chord.song_id
+  })
 
   if (!user) return <h1>loading data...</h1>
+
+  function handleChange(e) {
+    const name = e.target.name
+    const value = e.target.value
+    setChordFormData({
+      ...chordFormData,
+      [name]: value,
+    })
+  }
+
+  function handleChordEditSubmit(e) {
+    e.preventDefault()
+    console.log(chordFormData)
+  }
 
   function handleDeleteClick() {
     console.log(chord.id)
@@ -19,16 +42,65 @@ export default function ChordPage({ chord }) {
     console.log(updatedUser)
   }
 
+  function handleEditChord() {
+    setEditChord(!editChord)
+  }
+
   return (
 
     <>
-    <h1 key={chord.id}>{chord.name}</h1>
-    <h3>Notes: {chord.notes}</h3>
-    <h3>Inversion: {chord.inversion}</h3>
-    <p>Notes: {chord.comments}</p>
-    <br />
-    <button onClick={handleDeleteClick}>RemoveChord</button>
-  </>
+      <h1 key={chord.id}>{chord.name}</h1>
+      <h3>Notes: {chord.notes}</h3>
+      <h3>Inversion: {chord.inversion}</h3>
+      <p>Notes: {chord.comments}</p>
+      <br />
+      <button onClick={handleEditChord}>Edit Chord</button>
+      <div>
+        {!editChord ? null : 
+          <form onSubmit={handleChordEditSubmit}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={chordFormData.name}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="notes"
+              placeholder="Notes in Chord"
+              value={chordFormData.notes}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="inversion"
+              placeholder="Inverted Chord?"
+              value={chordFormData.inversion}
+              onChange={handleChange}
+            />            
+            <input
+              type="text"
+              name="comments"
+              placeholder="Comments"
+              value={chordFormData.comments}
+              onChange={handleChange}
+            />
+            <input
+              type="ted"
+              name="image_url"
+              placeholder="Image URL"
+              value={chordFormData.image}
+              onChange={handleChange}
+            />
+            <br />
+            <button>Update</button>
+          </form>
+        }
+      </div>
+      <br />
+      <button onClick={handleDeleteClick}>RemoveChord</button>
+    </>
   )
 }
 
