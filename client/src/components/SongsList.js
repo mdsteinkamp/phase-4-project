87e0b4eda32d06b073908a999f08d8d12a0d9b6e
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-10-04 18:26:14 -0700

    filtering unique songs in the SongList comp

--- client/src/components/SongsList.js
+++ client/src/components/SongsList.js
@@ -3,12 +3,24 @@ import { UserContext } from "./UserContext"
 
 export default function SongsList() {
   const {user, setUser} = useContext(UserContext)
+  console.log(user)
+
+  function onlyUnique(value, index, array) {
+    return array.indexOf(value) === index;
+  }
+
+  const uniqueSongs = user.chords.map(chord => chord.song)
+  console.log(uniqueSongs)
+  const uniques = [...new Set(uniqueSongs.map(item => item.title))]
+  console.log(uniques)
 
 
   return (
     <div>
       <h1>songs list comp</h1>
-      {user ? <h4>here are your songs</h4> : <h4>Please log in!</h4>}
+      {user ? <h4>here are your songs</h4> 
+
+      : <h4>Please log in!</h4>}
     </div>
   )
 }
