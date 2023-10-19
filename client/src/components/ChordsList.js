f2fe9103c3f0811eb36f5a37ba60232ad26ac6d0
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-10-18 22:09:05 -0700

    user model user_songs method returns uniq songs associated to user

--- client/src/components/ChordsList.js
+++ client/src/components/ChordsList.js
@@ -4,10 +4,12 @@ import ChordPage from "./ChordPage"
 
 export default function SongsList() {
   const {user} = useContext(UserContext)
+  console.log(user)
 
   if (!user) return <h1>Please log in!</h1>
 
   const sortedChords = user.chords.sort((a, b) => a.id - b.id)
+  console.log(user.chords)
 
   return (
     <div>
