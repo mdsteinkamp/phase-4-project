3f122af9c9bd98dc78ba90d11633c6fcd595112d
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-10-21 14:27:28 -0700

    changed to 'Your Songs' in NavBar comp

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,7 +7,7 @@ export default function NavBar() {
       <NavLink to="/">Home</NavLink>
       <NavLink to="/chords">Chords</NavLink>
       <NavLink to="/chords/new">Add Chord</NavLink>
-      <NavLink to="/songs">Songs</NavLink>
+      <NavLink to="/songs">Your Songs</NavLink>
     </span>
   )
 }
