555a33e2522c3b4d490614aa0c21e6fd397e8f92
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-11-05 13:07:08 -0800

    updated ChordCard import in ChordsList comp

--- client/src/components/ChordsList.js
+++ client/src/components/ChordsList.js
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { UserContext } from "./UserContext"
-import ChordPage from "./ChordCard"
+import ChordCard from "./ChordCard"
 
 export default function SongsList() {
   const {user} = useContext(UserContext)
@@ -14,7 +14,7 @@ export default function SongsList() {
       <h1>Chords list comp</h1>
       <h4>here are your chords</h4>
         <ul>{sortedChords.map(chord => (
-          <ChordPage key={chord.id} chord={chord} />
+          <ChordCard key={chord.id} chord={chord} />
           ))}
         </ul>
     </div>
